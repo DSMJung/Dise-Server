cdef1df4d2f8551cb92faf21a93d4f6090bd80a1
jyk1029 <jyk102999@dsm.hs.kr>
2022-07-19 08:05:23 +0900

    🎲 :: 프로젝트  생성

---
+++ src/main/java/com/example/dise/DiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dise/DiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
