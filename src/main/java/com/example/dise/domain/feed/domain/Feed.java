11ccabe614c1a7eeb3a7751082fdd03a87aff90d
kim seongwon <seongwon1290@dsm.hs.kr>
2022-07-19 09:47:41 +0900

    🎲 :: Feed 엔티티 생성

---
+++ src/main/java/com/example/dise/domain/feed/domain/Feed.java
@@ -0,0 +1,35 @@
+package com.example.dise.domain.feed.domain;
+
+import com.example.dise.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20, nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Feed(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+}
