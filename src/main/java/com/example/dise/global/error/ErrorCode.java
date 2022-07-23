30b5fc83a90f23d52cd9b08c6ef5731523302723
김주영 <jyk102999@dsm.hs.kr>
2022-07-23 21:31:14 +0900

    💻 :: 예외 추가

--- src/main/java/com/example/dise/global/error/ErrorCode.java
+++ src/main/java/com/example/dise/global/error/ErrorCode.java
@@ -9,13 +9,10 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
-    PASSWORD_MISS_MATCH(401, "비밀번호가 일치하지 않습니다."),
-    NOT_DELETE_COMMENT(401, "댓글을 삭제할 수 없습니다."),
-    NOT_MODIFY_COMMENT(401, "댓글을 수정할 수 없습니다"),
     EXPIRED_JWT(401, "토큰이 만료되었습니다."),
     INVALID_JWT(401, "토큰이 유효하지 않습니다."),
 
-    ID_NOT_FOUND(404, "계정를 찾을 수 없습니다."),
+    ACCOUNT_ID_NOT_FOUND(404, "계정을 찾을 수 없습니다."),
     USER_NOT_FOUND(404,"유저를 찾을 수 없습니다."),
     FEED_NOT_FOUND(404,"게시물을 찾을 수 없습니다."),
     COMMENT_NOT_FOUND(404, "댓글을 찾을 수 없습니다."),
