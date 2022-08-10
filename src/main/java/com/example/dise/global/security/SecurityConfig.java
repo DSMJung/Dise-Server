77427712dd2c9d1d2dcea12866ac4359dde39cdc
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-11 00:03:21 +0900

    🔄 :: SecurityConfig 코드구조 수정

--- src/main/java/com/example/dise/global/security/SecurityConfig.java
+++ src/main/java/com/example/dise/global/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.example.dise.global.config;
+package com.example.dise.global.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,27 +7,36 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-public class WebSecurityConfig{
+@Configuration
+public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
                 .csrf().disable()
                 .formLogin().disable()
                 .cors()
 
                 .and()
-
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+
+                .antMatchers("/signup").permitAll()
+                .antMatchers("/login").permitAll()
+
+                .anyRequest().authenticated()
+
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
