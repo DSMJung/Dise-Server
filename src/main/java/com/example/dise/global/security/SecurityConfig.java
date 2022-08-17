b236483652151a043729e0bec3a1bbefe48e49a8
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-17 12:52:57 +0900

    ✅ :: HttpMethod 추가

--- src/main/java/com/example/dise/global/security/SecurityConfig.java
+++ src/main/java/com/example/dise/global/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.dise.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,8 +28,8 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
 
-                .antMatchers("/signup").permitAll()
-                .antMatchers("/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/signup").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
 
                 .anyRequest().authenticated()
 
