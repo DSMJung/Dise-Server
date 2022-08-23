47858437eddadb78b1ccef2f985c843e8fffd044
김주영 <jyk102999@dsm.hs.kr>
2022-08-23 20:18:56 +0900

    🪓 :: Conflict 오류 수정

--- src/main/java/com/example/dise/domain/user/controller/UserController.java
+++ src/main/java/com/example/dise/domain/user/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.example.dise.domain.user.controller;
 
-import com.example.dise.domain.user.controller.dto.request.UpdateUserInfoRequest;
-import com.example.dise.domain.user.controller.dto.request.UserSignUpRequest;
-import com.example.dise.domain.user.service.UpdateUserInfoService;
-
 import com.example.dise.domain.user.service.UserSignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import com.example.dise.domain.user.controller.dto.request.UpdateUserInfoRequest;
+import com.example.dise.domain.user.controller.dto.request.UserSignUpRequest;
+import com.example.dise.domain.user.service.UpdateUserInfoService;
 
 import javax.validation.Valid;
 
@@ -20,16 +19,15 @@ public class UserController {
 
     private final UpdateUserInfoService updateUserInfoService;
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/signup")
-    public void signUp(@RequestBody @Valid UserSignUpRequest request) {
-        userSignUpService.signUp(request);
-    }
-
-
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping
     public void updateUser(@RequestBody @Valid UpdateUserInfoRequest request) {
         updateUserInfoService.excute(request);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    public void signUp(@RequestBody @Valid UserSignUpRequest request) {
+        userSignUpService.signUp(request);
     }
 }
