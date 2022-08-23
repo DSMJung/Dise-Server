c90479d09eeb32392d29a6b2dcba96917566e6ff
김주영 <jyk102999@dsm.hs.kr>
2022-08-23 20:03:43 +0900

    💻 :: 내 정보 수정 Api 구현

--- src/main/java/com/example/dise/domain/user/controller/UserController.java
+++ src/main/java/com/example/dise/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.dise.domain.user.controller;
 
+import com.example.dise.domain.user.controller.dto.request.UpdateUserInfoRequest;
 import com.example.dise.domain.user.controller.dto.request.UserSignUpRequest;
+import com.example.dise.domain.user.service.UpdateUserInfoService;
 import com.example.dise.domain.user.service.UserSignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,10 +16,17 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserSignUpService userSignUpService;
+    private final UpdateUserInfoService updateUserInfoService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public void signUp(@RequestBody @Valid UserSignUpRequest request) {
         userSignUpService.signUp(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping
+    public void updateUser(@RequestBody @Valid UpdateUserInfoRequest request) {
+        updateUserInfoService.excute(request);
     }
 }
