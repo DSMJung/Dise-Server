71c0c090d470e5ab48992d211d43c8ed31b9f72a
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-01 23:05:33 +0900

    💻 :: 댓글 생성 Api 구현

---
+++ src/main/java/com/example/dise/domain/comment/controller/CommentController.java
@@ -0,0 +1,20 @@
+package com.example.dise.domain.comment.controller;
+
+import com.example.dise.domain.comment.controller.dto.request.CreateCommentRequest;
+import com.example.dise.domain.comment.service.CreateCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+    private final CreateCommentService commentService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{feed-id}")
+    public void createComment(@PathVariable("feed-id") Integer id, CreateCommentRequest request) {
+        commentService.execute(id, request);
+    }
+}
---
+++ src/main/java/com/example/dise/domain/comment/controller/dto/request/CreateCommentRequest.java
@@ -0,0 +1,10 @@
+package com.example.dise.domain.comment.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateCommentRequest {
+    private String content;
+}
