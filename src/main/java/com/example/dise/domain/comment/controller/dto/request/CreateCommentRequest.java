3e9da1590b19e2875072290569aacee5515569c9
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-06 11:07:09 +0900

    ✅ :: @NotBlank로 수정

--- src/main/java/com/example/dise/domain/comment/controller/dto/request/CreateCommentRequest.java
+++ src/main/java/com/example/dise/domain/comment/controller/dto/request/CreateCommentRequest.java
@@ -3,12 +3,12 @@ package com.example.dise.domain.comment.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 public class CreateCommentRequest {
 
-    @NotNull(message = "내용은 null을 허용하지 않습니다.")
+    @NotBlank(message = "내용은 null을 허용하지 않습니다.")
     private String content;
 }
