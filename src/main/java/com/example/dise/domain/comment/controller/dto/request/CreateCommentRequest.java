a0716c75982244f3cd60b62a882bc97647ed64d8
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-06 23:45:14 +0900

    ✅ :: 1000자로 max값 설정

--- src/main/java/com/example/dise/domain/comment/controller/dto/request/CreateCommentRequest.java
+++ src/main/java/com/example/dise/domain/comment/controller/dto/request/CreateCommentRequest.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class CreateCommentRequest {
 
-    @NotBlank(message = "내용은 null을 허용하지 않습니다.")
+    @NotBlank
+    @Size(max = 1000, message = "내용은 1000자 이내이고 Null을 허용하지 않습니다.")
     private String content;
 }
