31b0fe2f1ec12e16ce92869426b438ce918d1a8a
김주영 <jyk102999@dsm.hs.kr>
2022-09-13 18:56:20 +0900

    💻 :: 카테고리별 목록 조회 Api 조회

--- src/main/java/com/example/dise/domain/feed/domain/repository/FeedRepository.java
+++ src/main/java/com/example/dise/domain/feed/domain/repository/FeedRepository.java
@@ -1,11 +1,14 @@
 package com.example.dise.domain.feed.domain.repository;
 
 import com.example.dise.domain.feed.domain.Feed;
+import com.example.dise.domain.feed.domain.type.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed, Integer> {
+
+    List<Feed> findByCategoryOrderByCreatedAtDesc(Category category);
 
     List<Feed> findAllByOrderById();
 
