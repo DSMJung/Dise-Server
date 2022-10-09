6af364f5d9645c2b9ec39aa635286d54e711067b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-09 15:09:30 +0900

    ✅ :: 비밀번호 인코딩

--- src/main/java/com/example/dise/domain/user/service/UpdatePasswordService.java
+++ src/main/java/com/example/dise/domain/user/service/UpdatePasswordService.java
@@ -4,6 +4,7 @@ import com.example.dise.domain.user.controller.dto.request.UpdatePasswordRequest
 import com.example.dise.domain.user.domain.User;
 import com.example.dise.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,11 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UpdatePasswordService {
     private final UserFacade userFacade;
+    private final PasswordEncoder encoder;
 
     @Transactional
     public void updatePassword(UpdatePasswordRequest request) {
         User user = userFacade.getCurrentUser();
         userFacade.checkPassword(user, request.getPassword());
-        user.updatePassword(request.getNewPassword());
+        user.updatePassword(encoder.encode(request.getNewPassword()));
     }
 }
