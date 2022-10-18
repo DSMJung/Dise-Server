f942beb38cef6ee68cfe3a883a663fc57be87ad5
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-18 18:19:40 +0900

    🪓 :: 기존 비번 정규식 삭제

--- src/main/java/com/example/dise/domain/user/controller/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/example/dise/domain/user/controller/dto/request/UpdatePasswordRequest.java
@@ -11,9 +11,6 @@ import javax.validation.constraints.Pattern;
 public class UpdatePasswordRequest {
 
     @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!/?@])[a-zA-Z0-9!/" +
-            "?@]{8,20}$",
-            message = "password는 소문자, 숫자, 특수문자가 포함되어야 하며 8자 이상 20자 이하여야 합니다.")
     private String password;
 
     @NotBlank(message = "new_password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
